d3600c5b5789e79e233e29e6b516ad1627d6e3d7
Raúl Damian <raul3e@comunidad.unam.mx>
2022-03-18 17:00:34 -0600

    FEAT: "Primer boceto con bootstrap"

---
+++ src/app/header-footer/header-footer.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-header-footer',
+  templateUrl: './header-footer.component.html',
+  styleUrls: [
+    './header-footer.component.css'
+    /** , '../../assets/bootstrap/css/bootstrap.min.css'*/
+  ]
+})
+export class HeaderFooterComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
