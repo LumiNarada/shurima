4483c3eb62629afe54c1ab737b42dbf008a56c89
Raúl Damian <raul3e@comunidad.unam.mx>
2022-03-25 01:11:30 -0600

    FEAT: "Rutas de Angular y vista de LogIn añadidos"

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,34 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router'
 
 import { AppComponent } from './app.component';
 import { PrincipalComponent } from './principal/principal.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { AcercaDeComponent } from './acerca-de/acerca-de.component';
+import { LogInComponent } from './log-in/log-in.component';
+
+const routes: Routes = [
+  { path:'', pathMatch:'full', redirectTo:'principal'},
+  { path:'principal',component:PrincipalComponent},
+  { path:'acercaDe',component:AcercaDeComponent},
+  { path:'login',component:LogInComponent},
+  { path:'**',component:PrincipalComponent}
+];
 
 @NgModule({
   declarations: [
     AppComponent,
     PrincipalComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    AcercaDeComponent,
+    LogInComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
