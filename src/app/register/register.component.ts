2bfd5f6ff4e502fead8fc90d7188cfc06383e6a4
Raúl Damian <raul3e@comunidad.unam.mx>
2022-03-29 18:46:31 -0600

    FEAT: "Vista de registro añadida sin conexiona  la base de datos"

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -11,57 +11,11 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
-  datos:any
-  email: string;
-  password: string;
-  personas:Persona[]=[]
-  formulario: FormGroup;
-  validE:boolean;
-  validP:boolean;
   constructor(public personasService:PersonasService, private cookieService: CookieService, public router: Router) { 
-    this.email="";
-    this.password="";
-    this.validE=false;
-    this.validP=false;
-    this.formulario=new FormGroup({
-      email: new FormControl('', [Validators.required, Validators.pattern("[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,15}$")]),
-      password: new FormControl('', [Validators.required]),
-    })
+
   }
   ngOnInit(): void {
-      this.getPersonas();
-      this.datos=[];
+
   }
-  getPersonas():void{
-    this.personasService.getPersonas().subscribe(response =>{
-      const{personas}= response;
-      this.personas = personas;
-    });
-  }
-  submit(){
-    if(this.formulario.valid==false){
-      if(this.formulario.controls['email'].valid==false){
-        this.validE=true;
-      }
-      if(this.formulario.controls['password'].valid==false){
-        this.validP=true;
-      }
-    }
-    else{
-      this.personas.forEach((key : any, val: any) => {
-        if (this.formulario.value.email==key["email"]){
-          if (this.formulario.value.password==key["pass"]){
-            this.personasService.crearCookies(this.formulario.value.email, this.formulario.value.passsword);
-          }
-        }
-      })
-      if (this.cookieService.get('User')){
-        this.router.navigateByUrl('/');
-        window.location.replace('./');
-      }
-      else{
-        window.alert("Usuario o contraseña incorrectos")
-      }
-    }
-  }
+
 }
